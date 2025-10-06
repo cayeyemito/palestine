236b26ad578e2a9762d94534b1510d31372b67e2
Caye <46819817+cayeyemito@users.noreply.github.com>
2025-10-06 14:16:25 +0200

    Ultima prueba

--- Palestiweb/utils/supabase.ts
+++ Palestiweb/utils/supabase.ts
@@ -1,10 +1,12 @@
-import { createClient } from "@supabase/supabase-js"
+// supabase.ts
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+// Accede a las variables de entorno de Vite
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Faltan variables de entorno de Supabase")
+  throw new Error("Faltan variables de entorno de Supabase");
 }
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
