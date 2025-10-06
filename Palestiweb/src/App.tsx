9a221cdd19b6d54db7e599bcb4636bbb82a7d1e0
Caye <46819817+cayeyemito@users.noreply.github.com>
2025-10-06 14:05:01 +0200

    cambiamos env

--- Palestiweb/src/App.tsx
+++ Palestiweb/src/App.tsx
@@ -263,6 +263,7 @@ export default function App() {
         </div>
         <div className="mt-10 max-w-4xl mx-auto">
           <Page/>
+          <h1></h1>
         </div>
       </main>
     </div>
